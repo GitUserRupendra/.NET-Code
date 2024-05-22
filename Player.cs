b1193c895ae61fe0ab20ffc6f84aca8529876d50
Rupendra Nakarmi <146559594+GitUserRupendra@users.noreply.github.com>
2024-05-22 16:58:38 +0545

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;class Player
+  {
+      public string name = "Rupendra";
+      public int health = 50;
+      public void setHealth(int h)
+      {
+          health = h;
+      }
+  }
